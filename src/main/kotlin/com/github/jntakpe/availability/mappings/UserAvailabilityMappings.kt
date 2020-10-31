a744442592b9636bb1b8c50aa544ddfaa16e0149
jntakpe <jntakpe@gmail.com>
2020-11-01 00:36:50 +0100

    feat: implements entity to response mapping

--- src/main/kotlin/com/github/jntakpe/availability/mappings/UserAvailabilityMappings.kt
+++ src/main/kotlin/com/github/jntakpe/availability/mappings/UserAvailabilityMappings.kt
@@ -3,7 +3,16 @@ package com.github.jntakpe.availability.mappings
 import com.github.jntakpe.availability.model.entity.UserAvailability
 import com.github.jntakpe.availability.model.entity.WorkArrangementType
 import com.github.jntakpe.availability.proto.UsersAvailability.DeclareAvailabilityRequest
+import com.github.jntakpe.availability.proto.UsersAvailability.WorkArrangement
+import com.github.jntakpe.availability.proto.UsersAvailabilityResponse
 import java.time.LocalDate
 
 fun DeclareAvailabilityRequest.toEntity() = UserAvailability(userId, LocalDate.parse(day), WorkArrangementType.valueOf(arrangement.name))
 
+fun UserAvailability.toResponse() = UsersAvailabilityResponse {
+    val entity = this@toResponse
+    userId = entity.userId
+    day = entity.day.toString()
+    arrangement = WorkArrangement.valueOf(entity.arrangement.name)
+    id = entity.id.toString()
+}
--- src/test/kotlin/com/github/jntakpe/availability/mappings/UserAvailabilityMappingsKtTest.kt
+++ src/test/kotlin/com/github/jntakpe/availability/mappings/UserAvailabilityMappingsKtTest.kt
@@ -1,10 +1,13 @@
 package com.github.jntakpe.availability.mappings
 
+import com.github.jntakpe.availability.dao.UserAvailabilityDao.PersistedData.JDOE_ID
+import com.github.jntakpe.availability.dao.UserAvailabilityDao.PersistedData.jdoeOnsite
 import com.github.jntakpe.availability.dao.UserAvailabilityDao.TransientData.MDOE_ID
 import com.github.jntakpe.availability.model.entity.UserAvailability
 import com.github.jntakpe.availability.model.entity.WorkArrangementType
 import com.github.jntakpe.availability.proto.DeclareAvailabilityRequest
-import com.github.jntakpe.availability.proto.UsersAvailability
+import com.github.jntakpe.availability.proto.UsersAvailability.WorkArrangement
+import com.github.jntakpe.availability.proto.UsersAvailabilityResponse
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -13,13 +16,31 @@ internal class UserAvailabilityMappingsKtTest {
 
     @Test
     fun `to entity should map request`() {
+        val date = LocalDate.of(2020, 10, 31)
         val request = DeclareAvailabilityRequest {
             userId = MDOE_ID
-            day = "2020/10/31"
-            arrangement = UsersAvailability.WorkArrangement.ONSITE
+            day = date.toString()
+            arrangement = WorkArrangement.ONSITE
         }
         val entity = request.toEntity()
-        val expected = UserAvailability(MDOE_ID, LocalDate.of(2020, 10, 31), WorkArrangementType.ONSITE)
-        assertThat(entity).usingRecursiveComparison().isEqualTo(expected).ignoringFields("id")
+        val expected = UserAvailability(MDOE_ID, date, WorkArrangementType.ONSITE)
+        assertThat(entity).usingRecursiveComparison()
+            .ignoringFields(UserAvailability::id.name, UserAvailability::arrangement.name)
+            .isEqualTo(expected)
+        assertThat(entity.id).isNotNull
+        assertThat(entity.arrangement.name).isEqualTo(expected.arrangement.name)
+    }
+
+    @Test
+    fun `to response should map entity`() {
+        val expected = UsersAvailabilityResponse {
+            userId = JDOE_ID
+            day = jdoeOnsite.day.toString()
+            arrangement = WorkArrangement.valueOf(jdoeOnsite.arrangement.name)
+            id = jdoeOnsite.id.toString()
+        }
+        val response = jdoeOnsite.toResponse()
+        assertThat(response).usingRecursiveComparison().ignoringFields(UserAvailability::arrangement.name).isEqualTo(expected)
+        assertThat(response.arrangement.name).isEqualTo(expected.arrangement.name)
     }
 }
